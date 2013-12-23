72dba247157e1a5350423660bfd9321705683025
Johann Kerr <johannkerr@gmail.com>
2013-12-22 20:17:41 -0500

    initialized to heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Difts::Application.config.secret_key_base = '6f0edb972670c7dc80760178eeaefb176b246b98e861177feb2746f4ec91585a2d376cdac03f77dc061de80353fe2c2d279d6b052130ca4d49442fd802f65d50'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Difts::Application.config.session_store :cookie_store, key: '_difts_session'
